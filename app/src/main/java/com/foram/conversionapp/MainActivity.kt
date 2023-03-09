fa3d21f5275dd2fd9c47341f0b55da40d4f60ffd
foramjpatel5778@gmail.com <foramjpatel5778@gmail.com>
2023-03-09 10:24:50 -0500

    Implemented logic for time conversion

--- app/src/main/java/com/foram/conversionapp/MainActivity.kt
+++ app/src/main/java/com/foram/conversionapp/MainActivity.kt
@@ -2,24 +2,31 @@ package com.foram.conversionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.foram.conversionapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private var sourceUnits =
-        arrayOf("Select Source Unit", "seconds", "minutes", "hours", "days", "weeks", "months")
+        arrayOf("Select Source Unit", "Seconds", "Minutes", "Hours", "Days", "Weeks", "Months")
     private var destinationUnits = arrayOf(
         "Select Destination Unit",
-        "seconds",
-        "minutes",
-        "hours",
-        "days",
-        "weeks",
-        "months",
-        "years"
+        "Seconds",
+        "Minutes",
+        "Hours",
+        "Days",
+        "Weeks",
+        "Months",
+        "Years"
     )
+
+    private lateinit var destinationAdapter: ArrayAdapter<String>
+
+    var calculatedAns = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,11 +34,127 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val sourceAdapter = ArrayAdapter(this, R.layout.units_dropdown_menu, sourceUnits)
-        val destinationAdapter = ArrayAdapter(this, R.layout.units_dropdown_menu, destinationUnits)
-
         binding.spinnerSourceUnit.adapter = sourceAdapter
+
+        destinationAdapter = ArrayAdapter(
+            this@MainActivity,
+            R.layout.units_dropdown_menu,
+            destinationUnits
+        )
         binding.spinnerDestinationUnit.adapter = destinationAdapter
 
+        binding.spinnerSourceUnit.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                }
 
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    when (p2) {
+                        0 -> disableDestinationUnitSpinner()
+                        1 -> {
+                            val answerArray = mutableListOf(
+                                0.0,
+                                1.0,
+                                0.01667,
+                                0.000278,
+                                0.00001157,
+                                0.00000165,
+                                0.00000038,
+                                0.00000003
+                            )
+                            setDestinationAdapter(p2, answerArray)
+                        }
+                        2 -> {
+                            val answerArray = mutableListOf(
+                                0.0, 60.0, 1.0, 0.01667, 0.0006944, 0.0009921, 0.00002283, 0.0000019
+                            )
+                            setDestinationAdapter(p2, answerArray)
+                        }
+                        3 -> {
+                            val answerArray = mutableListOf(
+                                0.0, 3600.0, 60.0, 1.0, 0.041667, 0.005952, 0.001369, 0.0001141
+                            )
+                            setDestinationAdapter(p2, answerArray)
+                        }
+                        4 -> {
+                            val answerArray = mutableListOf(
+                                0.0, 86400.0, 1440.0, 24.0, 1.0, 0.142857, 0.328763, 0.002739
+                            )
+                            setDestinationAdapter(p2, answerArray)
+                        }
+                        5 -> {
+                            val answerArray = mutableListOf(
+                                0.0, 604800.0, 10080.0, 168.0, 7.0, 1.0, 0.23014, 0.019178
+                            )
+                            setDestinationAdapter(p2, answerArray)
+                        }
+                        6 -> {
+                            val answerArray = mutableListOf(
+                                0.0, 2629746.0, 43800.0, 730.0, 30.417, 4.345, 1.0, 0.0833
+                            )
+                            setDestinationAdapter(p2, answerArray)
+                        }
+                    }
+                }
+            }
+
+//        binding.spinnerDestinationUnit.onItemSelectedListener =
+//            object : AdapterView.OnItemSelectedListener {
+//                override fun onNothingSelected(p0: AdapterView<*>?) {
+//                    TODO("Not yet implemented")
+//                }
+//
+//                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+//                    when (p2) {
+//                        1 -> {
+//
+//                        }
+//                    }
+//                }
+//            }
+    }
+
+    private fun setDestinationAdapter(index: Int, answerArray: MutableList<Double>) {
+        val copiedUnits = destinationUnits.copyOf().toMutableList()
+        copiedUnits.removeAt(index)
+        answerArray.removeAt(index)
+
+        destinationAdapter = ArrayAdapter(this, R.layout.units_dropdown_menu, copiedUnits)
+        binding.spinnerDestinationUnit.adapter = destinationAdapter
+
+        enableDestinationUnitSpinner()
+
+        binding.spinnerDestinationUnit.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                }
+
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    when (p2) {
+                        0 -> {
+
+                        }
+                        else -> {
+                            calculatedAns = answerArray[p2]
+                            println("Answer: $calculatedAns")
+                        }
+                    }
+                }
+            }
+
+    }
+
+    private fun disableDestinationUnitSpinner() {
+        binding.spinnerDestinationUnit.isEnabled = false
+        binding.spinnerDestinationUnit.isClickable = false
+    }
+
+    private fun enableDestinationUnitSpinner() {
+        binding.spinnerDestinationUnit.isEnabled = true
+        binding.spinnerDestinationUnit.isClickable = true
+    }
+
+    private fun printToastMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
