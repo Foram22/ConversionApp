7635d582d9a78366fa798a8ca6ac282773fa4d37
foramjpatel5778@gmail.com <foramjpatel5778@gmail.com>
2023-03-09 09:03:41 -0500

    Implemented two spinners for source and destination unit selection

--- app/src/main/java/com/foram/conversionapp/MainActivity.kt
+++ app/src/main/java/com/foram/conversionapp/MainActivity.kt
@@ -2,10 +2,36 @@ package com.foram.conversionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.foram.conversionapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var sourceUnits =
+        arrayOf("Select Source Unit", "seconds", "minutes", "hours", "days", "weeks", "months")
+    private var destinationUnits = arrayOf(
+        "Select Destination Unit",
+        "seconds",
+        "minutes",
+        "hours",
+        "days",
+        "weeks",
+        "months",
+        "years"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sourceAdapter = ArrayAdapter(this, R.layout.units_dropdown_menu, sourceUnits)
+        val destinationAdapter = ArrayAdapter(this, R.layout.units_dropdown_menu, destinationUnits)
+
+        binding.spinnerSourceUnit.adapter = sourceAdapter
+        binding.spinnerDestinationUnit.adapter = destinationAdapter
+
+
     }
 }
